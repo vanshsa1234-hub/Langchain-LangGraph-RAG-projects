b69b5646aaba7cf86ba44a75105f54ff2173206e
Vansh <vanshsa1234@gmail.com>
2026-09-16 02:57:52 +0530

    Complete SQL_agent, TaskBot - Manage your tasks

--- apps/4_sql_agent.py
+++ apps/4_sql_agent.py
@@ -4,6 +4,10 @@ load_dotenv()
 from langchain_groq import ChatGroq
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents import create_agent
+import streamlit as st
+
 
 db=SQLDatabase.from_uri("sqlite:///apps/my_tasks.db")
 db.run("""
@@ -16,5 +20,65 @@ db.run("""
     );
 """)
 
-model
-print("DB Created Successfully✅")
+
+#  llm , model, memory, system_prompt
+model=ChatGroq(model="openai/gpt-oss-20b")
+toolkit=SQLDatabaseToolkit(db=db, llm=model)
+tools=toolkit.get_tools()
+
+for tool in tools:
+    print(tool)
+
+system_prompt = """
+You are a task management assistant that interacts with a SQL database containing a 'tasks' table. 
+
+TASK RULES:
+1. Limit SELECT queries to 10 results max with ORDER BY created_at DESC
+2. After CREATE/UPDATE/DELETE, confirm with SELECT query
+3. If the user requests a list of tasks, present the output in a structured table format to ensure a clean and organized display in the browser."
+
+CRUD OPERATIONS:
+    CREATE: INSERT INTO tasks(title, description, status)
+    READ: SELECT * FROM tasks WHERE ... LIMIT 10
+    UPDATE: UPDATE tasks SET status=? WHERE id=? OR title=?
+    DELETE: DELETE FROM tasks WHERE id=? OR title=?
+
+Table schema: id, title, description, status(pending/in_progress/completed), created_at.
+"""
+
+@st.cache_resource
+def get_agent():
+    agent=create_agent(
+        model=model,
+        tools=tools,
+        checkpointer=InMemorySaver(),
+        system_prompt=system_prompt
+    )
+
+    return agent
+
+agent=get_agent()
+
+st.subheader("TaskBot - Manage your todos")
+
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).markdown(message["content"])
+
+prompt=st.chat_input("Ask me to manage your task?")
+if(prompt):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role":"user", "content": prompt})
+
+    with st.chat_message("ai"):
+        with st.spinner("Processing..."):
+            response=agent.invoke(
+                    {"messages":[{"role":"user","content":prompt}]},
+                    {"configurable":{"thread_id":"1"}}
+                )
+            result= response["messages"][-1].content
+            st.markdown(result)
+            st.session_state.messages.append({"role":"ai","content":result})
+
