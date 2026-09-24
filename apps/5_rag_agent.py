ebe534d7be6805f67ddb6f801047d251f470c4ac
Vansh <vanshsa1234@gmail.com>
2026-09-25 02:55:53 +0530

    Conplete the pending task in ai agent rag chatbot

--- apps/5_rag_agent.py
+++ apps/5_rag_agent.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -9,56 +9,96 @@ from langchain_community.vectorstores import InMemoryVectorStore
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langgraph.checkpoint.memory import InMemorySaver
+import streamlit as st
 
-## load document
-loader=PyPDFLoader("data/medical_report.pdf")
-docs=loader.load()
+# ## data in st session
+if "document_uploaded" not in st.session_state:
+    st.session_state.document_uploaded=False
 
-## split into chunks
-splitter= RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-docs=splitter.split_documents(documents=docs)
+if "agent" not in st.session_state:
+    st.session_state.agent=None
 
-## embedding and vector DB
-embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-vector_db=InMemoryVectorStore.from_documents(
-    documents=docs,
-    embedding=embeddings
-)
+if "vector_store" not in st.session_state:
+    st.session_state.vector_store=None
 
-## create a agent - tool, llm, prompt
-llm=ChatGroq(model="openai/gpt-oss-20b")
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
 
-@tool
-def retrieve_context(query:str):
-    """"
-        retrieve documents related to a query from the knowledge base.
-    """
-    context=""
-    docs=vector_db.similarity_search(query=query, k=3)
-    for doc in docs:
-        context += doc.page_content + "\n\n"
-    return context
 
-system_prompt = """You are a helpful assistant that answers questions using retrieved context. 
-        My knowledge base consists of the details from the uploaded document. 
-        ALWAYS use the `retrieve_context` tool for questions requiring external knowledge."""
 
-memory=InMemorySaver()
+def process_document(path):
+    ## load document
+    loader=PyPDFDirectoryLoader(path)
+    docs=loader.load()
 
-agent=create_agent(
-    model=llm,
-    tools=[retrieve_context],
-    system_prompt=system_prompt,
-    checkpointer=memory
-)
+    ## split into chunks
+    splitter= RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    docs=splitter.split_documents(documents=docs)
 
-while True:
-    query=input("User: ")
-    if query.lower()=="quit":
-        break
-    response=agent.invoke(
-        {"messages":[{"role":"user", "content":query}]},
-        {"configurable":{"thread_id":1}}
+    ## embedding and vector DB
+    embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_db=InMemoryVectorStore.from_documents(
+        documents=docs,
+        embedding=embeddings
+    )
+
+    ## create a agent - tool, llm, prompt
+    llm=ChatGroq(model="openai/gpt-oss-20b")
+
+    @tool
+    def retrieve_context(query:str):
+        """"
+            retrieve documents related to a query from the knowledge base.
+        """
+        context=""
+        docs=vector_db.similarity_search(query=query, k=3)
+        for doc in docs:
+            context += doc.page_content + "\n\n"
+        return context
+
+    system_prompt = """You are a helpful assistant that answers questions using retrieved context. 
+            My knowledge base consists of the details from the uploaded document. 
+            ALWAYS use the `retrieve_context` tool for questions requiring external knowledge."""
+
+    memory=InMemorySaver()
+
+    agent=create_agent(
+        model=llm,
+        tools=[retrieve_context],
+        system_prompt=system_prompt,
+        checkpointer=memory
+    )
+    st.session_state.agent=agent
+    st.session_state.document_uploaded=True
+
+# upload ui
+if not st.session_state.document_uploaded:
+    uploaded=st.file_uploader(label="Select PDF File", type=["pdf"], accept_multiple_files=True)
+    if uploaded:
+        with st.spinner("Processing..."):
+            path="./doc_files/"
+            for file in uploaded:
+                with open(path+file.name,"wb")as f:
+                    f.write(file.getvalue())
+
+            process_document(path)
+            st.rerun() 
+
+# chat ui  
+if st.session_state.document_uploaded and st.session_state.agent:
+    for message in st.session_state.messages:
+        role=message.get("role")
+        content=message.get("content")
+        st.chat_message(role).markdown(content)
+    query=st.chat_input("Ask anything related to uploaded document.... ")
+
+    if query:
+        st.session_state.messages.append({"role": "user", "content":query})
+        st.chat_message("user").markdown(query)
+        response=st.session_state.agent.invoke(
+            {"messages" : [{"role": "user", "content": query}]},
+            {"configurable": {"thread_id":1}}
         )
-    result= response["messages"][-1].content
-    print("AI :", result)
+        answer=response["messages"][-1].content
+        st.chat_message("ai").markdown(answer)
+        st.session_state.messages.append({"role": "ai", "content":answer})
